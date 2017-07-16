969b4375657343e3574a7f4de111ae6eb21cab15
sungmee <mo@lxooo.com>
2017-07-16 23:49:07 +0800

    fix bug

--- onbuild.sh
+++ onbuild.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#! /bin/sh
 cd /var/www
 if [ -f "composer.json" ]
 then
-    composer install --no-scripts
-    chmod -R 777 storage bootstrap/cache
+    exec /sbin/setuser www-data composer install --no-scripts
+    exec /sbin/setuser root chmod -R 777 storage bootstrap/cache
 else
     cp -a /var/www /var/www-data
     rm -rf *
     rm -rf .[^.]*
     cd /var
-    composer create-project --prefer-dist laravel/laravel www
+    exec /sbin/setuser www-data composer create-project --prefer-dist laravel/laravel www
     mv -r /var/www-backup /var/www/www-backup
     cd /var/www
-    chmod -R 777 storage bootstrap/cache
+    exec /sbin/setuser root chmod -R 777 storage bootstrap/cache
 fi

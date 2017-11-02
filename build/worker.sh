542249b506b99cc123c0ff1f2afbcbefbf217e0e
SUNGMEE <mo@lxooo.com>
2017-11-02 10:27:55 +0800

    add queues once.

--- build/worker.sh
+++ build/worker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /sbin/setuser www-data php /var/www/artisan queue:work --queue=high,low,default,emails --sleep=3 --tries=3 --daemon
+exec /sbin/setuser www-data php /var/www/artisan queue:work --queue=high,low,default,emails,once --sleep=3 --tries=3 --daemon
